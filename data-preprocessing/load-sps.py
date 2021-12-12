10ec159fa641266519cb8461dc7a8a0f3e2fdff0
James Sungjae Lee <odobenuseKR@gmail.com>
2021-12-12 15:16:50 +0900

    Update load-sps.py

--- data-preprocessing/load-sps.py
+++ data-preprocessing/load-sps.py
@@ -4,21 +4,20 @@ import pandas as pd
 from tqdm import tqdm
 
 session = boto3.session.Session(profile_name='dev-session')
-s3 = session.client('s3')
+s3_client = session.client('s3')
+s3_resource = session.resource('s3')
 
 BUCKET_SPS = 'sungjae-sps-data'
 
-pagenator = s3.get_paginator('list_objects').paginate(Bucket=BUCKET_SPS, Prefix='data/')
+pagenator = s3_client.get_paginator('list_objects').paginate(Bucket=BUCKET_SPS, Prefix='data/')
 pages = [page for page in pagenator]
 contents = [content['Contents'] for content in pages]
 flat_contents = [content for sublist in contents for content in sublist]
 sps_objects = [(x['Key'], x['LastModified']) for x in flat_contents if 'pkl' in x['Key']]
 
-s3 = session.resource('s3')
-
 sps_list = []
 for sps_filename, sps_datetime in tqdm(sps_objects):
-    sps_queries = pickle.loads(s3.Bucket(BUCKET_SPS).Object(sps_filename).get()['Body'].read())
+    sps_queries = pickle.loads(s3_resource.Bucket(BUCKET_SPS).Object(sps_filename).get()['Body'].read())
     for query in sps_queries:
         instance_type = query[1]
         scores = query[3]
