7ed1889e5121e623aed83f52922d1db65a2a52c2
James Sungjae Lee <odobenuseKR@gmail.com>
2022-01-02 15:15:01 +0900

    Create save-region-ami.py

---
+++ data-preprocessing/save-region-ami.py
@@ -0,0 +1,28 @@
+import boto3
+import pickle
+
+region_list_default = ['us-east-2', 'ap-south-1', 'us-west-2', 'ap-northeast-3', 'ap-southeast-1', 'ap-northeast-2', 'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'sa-east-1', 'ap-northeast-1', 'ap-southeast-2', 'us-east-1', 'us-west-1']
+region_ami_dict = {}
+
+for region in region_list_default:
+    print(region)
+    session = boto3.session.Session(profile_name='dev-session')
+    client = session.client('ec2', region_name=region)
+
+    response = client.describe_images(Owners=['amazon'],
+                                      Filters=[{
+                                          'Name': 'name',
+                                          'Values': ['amzn*']}])
+    
+    sorted_images = sorted(response['Images'], key=lambda i: i['CreationDate'], reverse=True)
+    for ami in sorted_images:
+        try:
+            desc = ami['Description']
+            ami_id = ami['ImageId']
+            if ('Amazon Linux 2 Kernel 5.10 AMI' in desc) and ('x86_64 HVM gp2' in desc):
+                region_ami_dict[region] = (ami_id, ami)
+                break
+        except:
+            continue
+            
+pickle.dump(region_ami_dict, open('region_ami_dict.pkl', 'wb'))
