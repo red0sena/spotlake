8734690209fd186d263e5a3352610501f07e4d82
wynter <gusdud0122@kookmin.ac.kr>
2022-07-13 11:14:06 +0900

    구글 sku crawler 개발

---
+++ data-collection/sku_scraper/sku_scraper/spiders/data_scraper.py
@@ -0,0 +1,40 @@
+from asyncore import write
+import scrapy
+from scrapy.exceptions import CloseSpider
+
+
+class DataScraperSpider(scrapy.Spider):
+    search = 'preemptible'
+    offset = 0
+    limit = 100
+
+    name = 'data_scraper'
+    allowed_domains = ['lepton.appspot.com']
+    start_urls = ['http://lepton.appspot.com/']
+
+    def start_requests(self):
+        while(self.offset <= 1600):
+            url = self.start_urls[0] + 'skus?format=skuPage&currency=USD&filter={}&offset={}&limit={}'.format(
+                self.search, self.offset, self.limit)
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # extract outer "skus"
+        data_string = response.body.decode()
+        skus_tmp = data_string.split('"skus":[', 1)[1]
+        skus = skus_tmp.split('],"last_updated"')[0]
+
+        self.log(len(skus))
+
+        # when skus is empty => finish
+        if len(skus) <= 0:
+            raise CloseSpider('Response is finished!')
+
+        # write raw data
+        with open('./raw_data/{}.txt'.format(self.search), 'a') as f:
+            f.write(data_string)
+
+        self.log('Filename : {}, Write New Request for offset {}'.format(
+            self.search, self.offset))
+
+        self.offset += self.limit
