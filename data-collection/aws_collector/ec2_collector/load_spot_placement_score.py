2a6ac1e58286f786d1ae41464bf5f03a1a37a33a
황재일 <chris@hwangjaeil-ui-MacBookPro.local>
2022-07-25 11:39:16 +0900

    create load_spot_placement_score.py

---
+++ data-collection/aws_collector/ec2_collector/load_spot_placement_score.py
@@ -0,0 +1,48 @@
+import boto3
+import pandas
+import pickle
+
+def get_sps():
+    # need to change file location
+    credentials = pickle.load(open('./user_cred_df.pkl', 'rb'))
+    workload = pickle.load(open('./bin_packed_workload.pickle', 'rb'))
+    sps_list = []
+
+    for i in range(len(workload)):
+        user = credentials.iloc[i]
+        access_id = user['AccessKeyId']
+        secret_key = user['SecretAccessKey']
+        # credentials start with spotrank+002@kookmin.ac.kr at index 0
+        user_num = credentials.index[i] + 2
+
+        session = boto3.session.Session(
+                aws_access_key_id=access_id,
+                aws_secret_access_key=secret_key)
+        ec2 = session.client('ec2', region_name='us-west-2')
+        
+        for query in workload[i]:
+            response = ec2.get_spot_placement_scores(
+                    InstanceTypes=[query[0]],
+                    TargetCapacity=1,
+                    SingleAvailabilityZone=True,
+                    RegionNames=query[1])
+            scores = response['SpotPlacementScores']
+            results = ({'user_num': user_num}, query[0], query[1], scores)
+            sps_list.append(results)
+
+    sps_info = {'InstanceType' : [],
+            'Region' : [],
+            'AvailabilityZoneId': [],
+            'SPS' : [],
+            'TimeStamp' : []}
+    for sps in sps_list:
+        for info in sps[3]:
+            sps_dict['TimeStamp'].append(now_time)
+            sps_dict['InstanceType'].append(sps[1])
+            sps_dict['Region'].append(info['Region'])
+            sps_dict['AvailabilityZoneId'].append(info['AvailabilityZoneId'])
+            sps_dict['SPS'].append(info['Score'])
+
+    sps_info = pd.DataFrame(sps_info)
+
+    return sps_df
