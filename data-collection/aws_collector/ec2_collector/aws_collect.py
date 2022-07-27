f28edb10160ae78165a49c4362e99fe56064925a
chris0765 <jaeil@kookmin.ac.kr>
2022-07-27 14:28:51 +0900

    add filtering code to find changed data

--- data-collection/aws_collector/ec2_collector/aws_collect.py
+++ data-collection/aws_collector/ec2_collector/aws_collect.py
@@ -1,11 +1,8 @@
 import boto3
 import pickle
-import json
 import pandas as pd
 import argparse
 from datetime import datetime, timedelta
-from botocore.config import Config
-from botocore.exceptions import ClientError
 
 from load_price import get_spot_price, get_ondemand_price
 from load_spot_placement_score import get_sps
@@ -23,4 +20,11 @@ spotinfo_df = get_spotinfo()
 sps_df = get_sps()
 
 # spotlake_df = JOIN(spot_price_df, ondemand_price_df, spotinfo_df, sps_df)
-# upload_timestream(spotlake_df, timestamp)
+# previous_df = pickle.load(open("./latest_df.pkl", "rb")) # load previous data from local file system
+# pickle.dump(spotlake_df, open("./latest_df.pkl", "wb")) # write current data to local file system
+
+# update_latest(spotlake_df, timestamp) # upload current data to S3
+
+# changed_df = compare(previous, current) # compare previous_df and current_df to extract changed rows
+
+# upload_timestream(changed_df, timestamp)
