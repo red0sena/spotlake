934b1135bcab9757e72f8154567451ccfd7d9ba3
chris0765 <jaeil@kookmin.ac.kr>
2022-07-27 14:58:53 +0900

    change path of collecting code

--- collection/collect.sh
+++ collection/collect.sh
@@ -1,5 +1,5 @@
 date=$(date '+%Y-%m-%d %H:%M')
 
-python3 aws_collect.py --timestamp $date &
-# python3 azure_collect.py --timestamp $date &
-# python3 gcp_collect.py --timestamp $date &
+python3 ./aws/ec2_collector/aws_collect.py --timestamp $date &
+# python3 ./azure/collector/azure_collect.py --timestamp $date &
+# python3 ./gcp/collector/gcp_collect.py --timestamp $date &
