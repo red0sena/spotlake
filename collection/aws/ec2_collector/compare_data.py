06afea1e738bd07996514efc189cf34ee19bac13
chris0765 <jaeil@kookmin.ac.kr>
2022-08-01 17:40:56 +0900

    performance optimization(use indexes to make dataframe)

--- collection/aws/ec2_collector/compare_data.py
+++ collection/aws/ec2_collector/compare_data.py
@@ -5,14 +5,14 @@ def compare(previous_df, current_df):
     previous_df['Feature'] = previous_df['SPS'] + ":" + previous_df['IF'] + ":" + previous_df['SpotPrice'] + ":" + previous_df['Savings']
     current_df['Workload'] = current_df['InstanceType'] + ":" + current_df['Region'] + ":" + current_df['AZ']
     current_df['Feature'] = current_df['SPS'] + ":" + current_df['IF'] + ":" + current_df['SpotPrice'] + ":" + current_df['Savings']
-    changed_list = []
+    changed_idx_list = []
     for idx, row in current_df.iterrows():
         cmp = previous_df[previous_df['Workload'] == row['Workload']]
         if len(cmp) == 0:
-            changed_list.append(dict(row))
+            changed_idx_list.append(idx)
             continue
         cmp = cmp.iloc[0]
         if cmp['Feature'] != row['Feature']:
-            changed_list.append(dict(row))
-    changed_df = pd.DataFrame(changed_list)
+            changed_idx_list.append(idx)
+    changed_df = current_df.loc[changed_idx_list]
     return changed_df[changed_df.columns.difference(['Workload', 'Feature'])]
