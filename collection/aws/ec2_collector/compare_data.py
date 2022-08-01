59ca5447e64fa8d2ba458ae57665c45fc80bfa37
chris0765 <jaeil@kookmin.ac.kr>
2022-08-01 15:02:05 +0900

    create compare module

---
+++ collection/aws/ec2_collector/compare_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def compare(pervious_df, current_df):
+    changed_list = []
+    for idx, row in current_df.iterrows():
+        cmp = pervious_df[((row['InstanceType']==pervious_df['InstanceType']) & 
+                         (row['Region']==pervious_df['Region']) & 
+                         (row['AZ']==pervious_df['AZ']))]
+        if len(cmp) == 0:
+            changed_list.append(dict(row))
+            continue
+        cmp = cmp.iloc[0]
+        if cmp['SPS']!=row['SPS'] or cmp['IF']!=row['IF'] or cmp['SpotPrice']!=row['SpotPrice']:
+            changed_list.append(dict(row))
+    changed_df = pd.DataFrame(changed_list)
+    return changed_df
