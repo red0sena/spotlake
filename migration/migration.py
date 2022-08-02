bffecfb1642e78eda47280dcaccf62b2a7d3d9c3
James Sungjae Lee <sungjae@kookmin.ac.kr>
2022-08-02 15:06:54 +0900

    Update migration.py
    update version 1.0 of migration

--- migration/migration.py
+++ migration/migration.py
@@ -2,30 +2,38 @@ import os
 import time
 import boto3
 import tsquery
+import tsupload
 import pandas as pd
 
+
+SAVE_FILENAME = 'latest.csv.gz'
+PROFILE_NAME = 'default'
 BUCKET_NAME = 'spotlake'
+START_DATE = '2022-01-01'
+END_DATE = '2022-08-01'
+
 
 # compress data as gzip file, save to local file system, upload file to s3
-def save_gz_s3(df, timestamp, filename):
+def save_gz_s3(df, timestamp):
     # compress and save to LFS
-    df.to_csv(filename, index=False, compression="gzip")
+    df.to_csv(SAVE_FILENAME, index=False, compression="gzip")
     
     # upload compressed file to S3
-    session = boto3.Session(profile_name='sungjae')
+    session = boto3.Session(profile_name=PROFILE_NAME)
     s3 = session.client('s3')
     s3_dir_name = '/'.join(timestamp.split('T')[0].split('-'))
     s3_obj_name = timestamp.split('T')[1]
     
-    with open(filename, 'rb') as f:
+    with open(SAVE_FILENAME, 'rb') as f:
         s3.upload_fileobj(f, BUCKET_NAME, f"rawdata/{s3_dir_name}/{s3_obj_name}.csv.gz")
+
 
 # compare previous_df and current_df for changed row detection
 def compare(previous_df, current_df):
     previous_df['Workload'] = previous_df['InstanceType'] + ":" + previous_df['Region'] + ":" + previous_df['AZ']
-    previous_df['Feature'] = previous_df['SPS'] + ":" + previous_df['IF'] + ":" + previous_df['SpotPrice'] + ":" + previous_df['Savings']
+    previous_df['Feature'] = previous_df['SPS'].astype(str) + ":" + previous_df['IF'] + ":" + previous_df['Savings'].astype(str)
     current_df['Workload'] = current_df['InstanceType'] + ":" + current_df['Region'] + ":" + current_df['AZ']
-    current_df['Feature'] = current_df['SPS'] + ":" + current_df['IF'] + ":" + current_df['SpotPrice'] + ":" + current_df['Savings']
+    current_df['Feature'] = current_df['SPS'].astype(str) + ":" + current_df['IF'] + ":" + current_df['Savings'].astype(str)
     changed_idx_list = []
     for idx, row in current_df.iterrows():
         cmp = previous_df[previous_df['Workload'] == row['Workload']]
@@ -38,26 +46,20 @@ def compare(previous_df, current_df):
     changed_df = current_df.loc[changed_idx_list]
     return changed_df[changed_df.columns.difference(['Workload', 'Feature'])]
 
-# write changed row dataframe to tiemstream
-def write_timestream(data):
-    # write
 
 if __name__ == "__main__":
     # get every unique timestamp information as a list
-    start_date = '2022-01-01'
-    end_date = '2022-08-01'
-    timestamp_list = tsquery.get_timestamps(start_date, end_date)
+    timestamp_list = tsquery.get_timestamps(START_DATE, END_DATE)
 
-    for timestamp in timestamp_list[:2]:
+    for timestamp in timestamp_list[:10]:
         timestamp = 'T'.join(timestamp.split())
-        print(timestamp)
         current_df = tsquery.get_timestream(timestamp, timestamp)
 
-        if 'latest_df.pkl' not in os.listdir('./'):
-            save_gz_s3(current_df, timestamp, 'latest.csv.gz')
-            write_timestream(current_df)
-        else:    
-            previous_df = pd.read_csv('latest.csv.gz', compression='gzip', header=0, sep=',', quotechar='"', error_bad_lines=False)
-            save_gz_s3(current_df, timestamp, 'latest.csv.gz')
+        if SAVE_FILENAME not in os.listdir('./'):
+            save_gz_s3(current_df, timestamp)
+            tsupload.upload_timestream(current_df, PROFILE_NAME)
+        else:
+            previous_df = pd.read_csv(SAVE_FILENAME, compression='gzip', header=0, sep=',', quotechar='"')
+            save_gz_s3(current_df, timestamp)
             changed_df = compare(previous_df, current_df)
-            write_timestream(changed_df)
+            tsupload.upload_timestream(changed_df, PROFILE_NAME)
