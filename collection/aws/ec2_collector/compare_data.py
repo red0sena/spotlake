e62fe49bf3e7c6d6e623215768a084f9e28f5ec7
chris0765 <jaeil@kookmin.ac.kr>
2022-08-02 15:02:14 +0900

    change type of previous_df to json

--- collection/aws/ec2_collector/compare_data.py
+++ collection/aws/ec2_collector/compare_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 def compare(previous_df, current_df):
+    previous_df = pd.DataFrame(eval(previous_df))
     previous_df['Workload'] = previous_df['InstanceType'] + ":" + previous_df['Region'] + ":" + previous_df['AZ']
     previous_df['Feature'] = previous_df['SPS'] + ":" + previous_df['IF'] + ":" + previous_df['SpotPrice'] + ":" + previous_df['Savings']
     current_df['Workload'] = current_df['InstanceType'] + ":" + current_df['Region'] + ":" + current_df['AZ']
-    current_df['Feature'] = current_df['SPS'] + ":" + current_df['IF'] + ":" + current_df['SpotPrice'] + ":" + current_df['Savings']
+    current_df['Feature'] = current_df['SPS'] + ":" + current_df['IF'] + ":" + current_df['Savings']
     changed_idx_list = []
     for idx, row in current_df.iterrows():
         cmp = previous_df[previous_df['Workload'] == row['Workload']]
