2b90f7cb35017ab7a398ae46afc3b921cb66b9f7
James Sungjae Lee <sungjae@kookmin.ac.kr>
2022-08-12 14:04:36 +0900

    check removed workload, add 'Ceased' column

--- migration/migration.py
+++ migration/migration.py
@@ -56,13 +56,13 @@ def compare_nparray(previous_df, current_df, workload_cols, feature_cols):
     current_df.loc[:,'Workload'] = current_df[workload_cols].apply(lambda row: ':'.join(row.values.astype(str)), axis=1)
     current_df.loc[:,'Feature'] = current_df[feature_cols].apply(lambda row: ':'.join(row.values.astype(str)), axis=1)
 
-
     current_indices = current_df[['Workload', 'Feature']].sort_values(by='Workload').index
     current_values = current_df[['Workload', 'Feature']].sort_values(by='Workload').values
     previous_indices = previous_df[['Workload', 'Feature']].sort_values(by='Workload').index
     previous_values = previous_df[['Workload', 'Feature']].sort_values(by='Workload').values
     
     changed_indices = []
+    removed_indices = []
     
     prev_idx = 0
     curr_idx = 0
@@ -72,6 +72,7 @@ def compare_nparray(previous_df, current_df, workload_cols, feature_cols):
         elif curr_idx == len(current_indices):
             prev_workload = previous_values[prev_idx][0]
             if prev_workload not in current_values[:,0]:
+                removed_indices.append(previous_indices[prev_idx])
                 prev_idx += 1
                 continue
             else:
@@ -98,8 +99,8 @@ def compare_nparray(previous_df, current_df, workload_cols, feature_cols):
                 changed_indices.append(current_indices[curr_idx])
                 curr_idx += 1
             elif prev_workload not in current_values[:,0]:
+                removed_indices.append(previous_indices[prev_idx])
                 prev_idx += 1
-                continue
             else:
                 raise Exception('workload error')
         else:
@@ -107,7 +108,15 @@ def compare_nparray(previous_df, current_df, workload_cols, feature_cols):
                 changed_indices.append(current_indices[curr_idx])
             curr_idx += 1
             prev_idx += 1
-    return current_df.loc[changed_indices].drop(['Workload', 'Feature'], axis=1)
+    
+    changed_df = current_df.loc[changed_indices].drop(['Workload', 'Feature'], axis=1)
+    removed_df = previous_df.loc[removed_indices].drop(['Workload', 'Feature'], axis=1)
+    for col in feature_cols:
+        removed_df[col] = None
+
+    # removed_df have one more column, 'Ceased'
+    removed_df['Ceased'] = True
+    return changed_df, removed_df
 
 
 def date_range(start, end):
@@ -115,8 +124,10 @@ def date_range(start, end):
     days = [start + timedelta(days=i) for i in range(delta.days + 1)]
     return days
 
+
 def time_format(timestamp):
     return 'T'.join(str(timestamp).split())
+
 
 days = date_range(start_date, end_date)
 
@@ -153,9 +164,11 @@ for idx in range(len(days)-1):
             save_gz_s3(current_df, timestamp)
             print(f"elapsed time - read and save: {time.time() - perf_start}")
             perf_start = time.time()
-            changed_df = compare_nparray(previous_df, current_df, workload_cols, feature_cols)
+            changed_df, removed_df = compare_nparray(previous_df, current_df, workload_cols, feature_cols)
             print(f"elapsed time - compare: {time.time() - perf_start}")
             perf_start = time.time()
+            # changed_df and removed_df have different shape, because of 'Ceased' column
             tsupload.upload_timestream(changed_df)
+            tsupload.upload_timestream(removed_df)
             print(f"elapsed time - upload: {time.time() - perf_start}")
 print(f"elapsed time - total single day: {time.time() - perf_start_total}")
