a8b66b7ab187f1936e743238180f1926b4f8bc91
Ubuntu <ubuntu@ip-172-31-49-245.us-west-2.compute.internal>
2022-11-27 08:31:11 +0000

    remove duplicated module

--- collector/spot-dataset/aws/ec2_collector/aws_collect.py
+++ collector/spot-dataset/aws/ec2_collector/aws_collect.py
@@ -3,6 +3,7 @@ import botocore
 import pickle
 import pandas as pd
 import argparse
+import sys
 import os
 import time
 from datetime import datetime, timedelta
@@ -15,6 +16,9 @@ from load_spotinfo import get_spotinfo
 from compare_data import compare
 from upload_data import upload_timestream, update_latest, save_raw
 from join_data import build_join_df
+
+sys.path.append('/home/ubuntu/spotlake/utility')
+
 from slack_msg_sender import send_slack_message
 
 NUM_CPU = 2
@@ -41,31 +45,51 @@ credentials = pickle.load(open(f'{LOCAL_PATH}/user_cred_df_100_199.pkl', 'rb'))
 mp_workload = []
 for i in range(len(workload)):
     mp_workload.append((credentials.index[i], credentials.iloc[i], workload[i]))
+try:
+    session = boto3.session.Session()
+    regions = get_regions(session)
+    spot_price_df_list = []
+    with Pool(NUM_CPU) as p:
+        spot_price_df_list = p.map(get_spot_price, regions)
+    spot_price_df = pd.concat(spot_price_df_list).reset_index(drop=True)
+except botocore.exceptions.ClientError as e:
+    send_slack_message(e)
 
-session = boto3.session.Session()
-regions = get_regions(session)
-spot_price_df_list = []
-with Pool(NUM_CPU) as p:
-    spot_price_df_list = p.map(get_spot_price, regions)
-spot_price_df = pd.concat(spot_price_df_list).reset_index(drop=True)
+try:
+    ondemand_price_df = get_ondemand_price(date)
+except botocore.exceptions.ClientError as e:
+    send_slack_message(e)
 
-ondemand_price_df = get_ondemand_price(date)
+try:
+    spotinfo_df = get_spotinfo()
+except botocore.exceptions.ClientError as e:
+    send_slack_message(e)
 
-spotinfo_df = get_spotinfo()
+try:
+    sps_df_list = []
+    with Pool(NUM_CPU) as p:
+        sps_df_list = p.map(get_sps, mp_workload)
+    sps_df = pd.concat(sps_df_list).reset_index(drop=True)
+except botocore.exceptions.ClientError as e:
+    send_slack_message(e)
 
-sps_df_list = []
-with Pool(NUM_CPU) as p:
-    sps_df_list = p.map(get_sps, mp_workload)
-sps_df = pd.concat(sps_df_list).reset_index(drop=True)
+try:
+    current_df = build_join_df(spot_price_df, ondemand_price_df, spotinfo_df, sps_df)
+except Exception as e:
+    send_slack_message(e)
 
-current_df = build_join_df(spot_price_df, ondemand_price_df, spotinfo_df, sps_df)
-
-update_latest(current_df, timestamp) # upload current data to S3
-save_raw(current_df, timestamp)
+try:
+    update_latest(current_df, timestamp) # upload current data to S3
+    save_raw(current_df, timestamp)
+except botocore.exceptions.ClientError as e:
+    send_slack_message(e)
 
 if 'latest_df.pkl' not in os.listdir(f'{LOCAL_PATH}/'):
     pickle.dump(current_df, open(f"{LOCAL_PATH}/latest_df.pkl", "wb"))
-    upload_timestream(current_df, timestamp)
+    try:
+        upload_timestream(current_df, timestamp)
+    except botocore.exceptions.ClientError as e:
+        send_slack_message(e)
     exit()
 
 previous_df = pickle.load(open(f"{LOCAL_PATH}/latest_df.pkl", "rb")) # load previous data from local file system
@@ -76,6 +100,9 @@ feature_cols = ['SPS', 'IF', 'SpotPrice', 'OndemandPrice']
 
 changed_df, removed_df = compare(previous_df, current_df, workload_cols, feature_cols) # compare previous_df and current_df to extract changed rows
 
-upload_timestream(changed_df, timestamp)
-upload_timestream(removed_df, timestamp)
+try:
+    upload_timestream(changed_df, timestamp)
+    upload_timestream(removed_df, timestamp)
+except botocore.exceptions.ClientError as e:
+    send_slack_message(e)
 
